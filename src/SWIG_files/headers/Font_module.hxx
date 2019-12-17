37111fe4cb7959c8c08f1f0e808cab577bbea646
Thomas Paviot <tpaviot@gmail.com>
2019-12-17 09:58:34 +0100

    Added Font headers

---
+++ src/SWIG_files/headers/Font_module.hxx
@@ -0,0 +1,36 @@
+#ifndef FONT_HXX
+#define FONT_HXX
+
+/*
+Copyright 2008-2019 Thomas Paviot (tpaviot@gmail.com)
+
+This file is part of pythonOCC.
+pythonOCC is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pythonOCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include<Font_BRepFont.hxx>
+#include<Font_BRepTextBuilder.hxx>
+#include<Font_FTFont.hxx>
+#include<Font_FTLibrary.hxx>
+#include<Font_FontAspect.hxx>
+#include<Font_FontMgr.hxx>
+#include<Font_NListOfSystemFont.hxx>
+#include<Font_NameOfFont.hxx>
+#include<Font_Rect.hxx>
+#include<Font_StrictLevel.hxx>
+#include<Font_SystemFont.hxx>
+#include<Font_TextFormatter.hxx>
+#include<Font_UnicodeSubset.hxx>
+
+#endif // FONT_HXX
